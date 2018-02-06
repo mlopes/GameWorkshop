b782082cb7a0a422ba34defef9adb7a369293dea
Marco Lopes <marco.paulo.lopes@gmail.com>
2018-02-06 14:28:30 +0000

    Make ship move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float accelaration = 50f;
+	public float rotationFactor = 50f;
+		
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () {
+		Debug.Log (rb.velocity);
+		bool isAccelarating = Input.GetKey (KeyCode.W);
+		bool isRotatingLeft = Input.GetKey (KeyCode.A);
+		bool isRotatingRight = Input.GetKey (KeyCode.D);
+
+		if (!rb.velocity.Equals (Vector3.zero)) {
+			if (isRotatingLeft) {
+				transform.Rotate (new Vector3(0, -1, 0) * rotationFactor * Time.deltaTime);
+			} else if (isRotatingRight) {
+				transform.Rotate (new Vector3(0, 1, 0) * rotationFactor * Time.deltaTime);
+			}
+		}
+
+		if (isAccelarating) {
+			rb.AddForce (transform.forward * accelaration);
+		}
+	}
+}
