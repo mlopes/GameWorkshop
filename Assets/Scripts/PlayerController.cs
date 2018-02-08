e7a52cf9792814cd9bd600fd28df366eac2b83b8
Marco Lopes <marco.paulo.lopes@gmail.com>
2018-02-08 21:52:35 +0000

    Ships now fire shots

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	[Header("Ship Settings")]
 	public float acceleration = 50f;
 	public float maximumSpeed = 10f;
 	public float rotationFactor = 50f;
+
+	[Header("Fire Settings")]
+	public float fireRate;
+
+	[Header("Associated Objects")]
+	public GameObject shot;
+	public Transform shotSpawn;
+
 		
 	private Rigidbody rb;
 	private string horizontalAxis;
 	private string verticalAxis;
+	private string fireButton;
+
+	private float timeWhenItCanFireAfain;
 
 	void Start()
 	{
@@ -19,9 +31,19 @@ public class PlayerController : MonoBehaviour {
 		if(name == "Player1") {
 			horizontalAxis = "Horizontal";
 			verticalAxis = "Vertical";
+			fireButton = "Fire1";
 		} else {
 			horizontalAxis = "HorizontalP2";
 			verticalAxis = "VerticalP2";
+			fireButton = "Fire2";
+		}
+	}
+
+	void Update()
+	{
+		if (Input.GetButton(fireButton) && Time.time > timeWhenItCanFireAfain) {
+			timeWhenItCanFireAfain = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
 		}
 	}
 
