75e977d0b3f55675026ca1a85158d061c7f2719a
Marco Lopes <marco.paulo.lopes@gmail.com>
2018-02-08 21:19:12 +0000

    Create shots

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	public float speed = 10f;
+
+	void Start () {
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		rb.velocity = transform.forward * speed;
+	}
+}
