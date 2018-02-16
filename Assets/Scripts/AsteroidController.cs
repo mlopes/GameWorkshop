ded260cc441d2fe52afc14b1ab009cca588ae283
Marco Lopes <marco.paulo.lopes@gmail.com>
2018-02-16 10:51:21 +0000

    Respawn players, and add an asteroid

---
+++ Assets/Scripts/AsteroidController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidController : MonoBehaviour
+{
+	public float tumble = .8f;
+
+	void Start ()
+	{
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		rb.angularVelocity = Random.insideUnitSphere * tumble; 
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,17 +11,26 @@ public class GameController : MonoBehaviour {
 	public Text player1ScoreText;
 	public Text player2ScoreText;
 
+	public float respawnTime = 1.2f;
+
 	private GameObject player1;
 	private GameObject player2;
 
 	private int player1Score;
 	private int player2Score;
 
+	private bool respawnPlayer1;
+	private bool respawnPlayer2;
+	private float player1RespawnTimer;
+	private float player2RespawnTimer;
+
 	void Start () {
 		player1Score = 0;
 		player2Score = 0;
 		player1ScoreText.text = "Player1: " + player1Score;
 		player2ScoreText.text = "Player2: " + player2Score;
+		respawnPlayer1 = false;
+		respawnPlayer2 = false;
 
 		player1 = (GameObject)Instantiate (player1Prefab, new Vector3 (-2, 0, 0), Quaternion.identity);
 		player2 = (GameObject)Instantiate (player2Prefab, new Vector3 (2, 0, 0), Quaternion.identity);	
@@ -30,8 +39,14 @@ public class GameController : MonoBehaviour {
 	public void PlayerGotKilled(string whichPlayer) {
 		if(whichPlayer == "Player1") {
 			UpdatePlayer2Score ();
+
+			respawnPlayer1 = true;
+			player1RespawnTimer = Time.time + respawnTime;
 		} else {
 			UpdatePlayer1Score ();
+
+			respawnPlayer2 = true;
+			player2RespawnTimer = Time.time + respawnTime;
 		}
 	}
 
@@ -43,5 +58,17 @@ public class GameController : MonoBehaviour {
 	void UpdatePlayer2Score() {
 		player2Score += 1;
 		player2ScoreText.text = "Player2: " + player2Score;
+	}
+
+	void Update() {
+		if(respawnPlayer1 && Time.time >= player1RespawnTimer) {
+			respawnPlayer1 = false;
+			player1 = (GameObject)Instantiate (player1Prefab, new Vector3 (-2, 0, 0), Quaternion.identity);
+		}
+
+		if(respawnPlayer2 && Time.time >= player2RespawnTimer) {
+			respawnPlayer2 = false;
+			player2 = (GameObject)Instantiate (player2Prefab, new Vector3 (2, 0, 0), Quaternion.identity);
+		}
 	}
 }
